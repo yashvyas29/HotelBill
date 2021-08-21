22bc33f22d36e24b664bf7d5c63b5b169bfc3690
Yash Vyas <vyasyas@hilti.com>
2021-08-21 22:45:04 +0530

    Initial Commit

---
+++ HotelBill/HotelBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBillApp.swift
+//  HotelBill
+//
+//  Created by global on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HotelBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
